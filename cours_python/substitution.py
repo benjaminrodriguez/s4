61c1567c72f31e6d89ca051fe658c426ea235d99
Benjamin Rodriguez <benjamin-rodriguez@outlook.fr>
2019-10-23 14:39:18 +0200

    first

--- cours_python/substitution.py
+++ cours_python/substitution.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python3
 
-import string
 import random
-
-message_dechiffre = "Bonjour tout le monde, comment tu vas"
-
-lettre = string.ascii_uppercase
-random.shuffle(lettre)
-key = print(''.join(lettre))
+import string
 
 def chiffre_substitution(key: str, msg: str) -> str:
-    i = 0
-    j = 1 
-    for i in str(lettre):
-        lettre[i] = message_chiffre[j]
-        j+=1
-        i+=1
-    pass
-print(message_dechiffre)
+    crypt_msg = ''
+    msg = msg.upper()
+    for letter in msg:
+        if letter.isalpha():
+            pos = ord(letter) - ord('A')
+            crypt_msg += key[pos]
+        else:
+            crypt_msg += letter
+    return crypt_msg
+
+def dechiffre_substitutioin(key: str, msg: str) -> str:
+    cleartext = ''
+    msg = msg.upper()
+    for letter in msg:
+        if letter.isalpha():
+            pos = key.index(letter)
+            cleartext += chr(pos + ord('A'))
+        else:
+            cleartext += letter
+    return cleartext
+
+
+# Generate 'unique' key:
+alphabet = [l for l in string.ascii_uppercase]
+random.shuffle(alphabet)
+key = ''.join(alphabet)
+
+
+msg_clair = "Coucou tout le monde"
+msg_chiffre = chiffre_substitution(key, msg_clair)
+print(msg_chiffre)
+msg_clair = dechiffre_substitutioin(key, msg_chiffre)
+print(msg_clair)
